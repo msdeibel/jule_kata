6730e24eab3c7bfab8c23bc248c9cf1b9fa94dbb
msdeibel <git2014@markus-deibel.de>
2014-08-31 19:20:47 +0200

    Added a first C# version with a few rules

---
+++ csharp_2013/JuleRules/JuleKataTests/RulesTests.cs
@@ -0,0 +1,38 @@
+﻿using JuleKata;
+using JuleKataTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace JuleKataTests
+{
+    [TestClass]
+    public class RulesShould
+    {
+        [TestMethod]
+        public void Return1To6ForARolledDie()
+        {
+            Assert.IsTrue(1 <= Rules.RollDie()
+                            && 6 >= Rules.RollDie());
+        }
+
+        [TestMethod]
+        public void SortThreeDiceFromHighToLow()
+        {
+            CollectionAssert.AreEqual(new int[] { 6, 2, 1 }, Rules.SortDice(1, 6, 2));
+            CollectionAssert.AreEqual(new int[] { 1, 1, 1 }, Rules.SortDice(1, 1, 1));
+        }
+
+        [TestMethod]
+        public void ShouldTransformDiceResultIntoCorrectInteger()
+        {
+            Assert.AreEqual(621, Rules.CalculateDiceValue(1,6,2));
+        }
+
+        [TestMethod]
+        public void Recognize111asThree()
+        {
+            Assert.IsTrue(Rules.IsThree(111));
+        }
+    }
+}
---
+++ csharp_2013/JuleRules/JuleRules/Rules.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuleKata
+{
+    public class Rules
+    {
+        public static int RollDie()
+        {
+            Random dieRoll = new Random();
+
+            return dieRoll.Next(1, 6);
+        }
+
+        public static int[] SortDice(int dieOneValue, int dieTwoValue, int dieThreeValue)
+        {
+            int[] workingArray = new int[] { dieOneValue, dieTwoValue, dieThreeValue };
+            Array.Sort(workingArray);
+            Array.Reverse(workingArray);
+
+            return workingArray;
+        }
+
+        public static bool IsThree(int diceValue)
+        {
+            return 111 == diceValue;
+        }
+
+        public static int CalculateDiceValue(int p1, int p2, int p3)
+        {
+            int[] singleDieValues = Rules.SortDice(p1, p2, p3);
+
+            string joinedDiceValues = String.Join("", new List<int>(singleDieValues).ConvertAll(i => i.ToString()).ToArray());
+
+            return Convert.ToInt32(joinedDiceValues);
+        }
+    }
+}
