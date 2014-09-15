960691122c2d7c3d06b8a54ea38d8eb11305366b
msdeibel <git2014@markus-deibel.de>
2014-09-15 21:30:11 +0200

    Adds dice value recognition and marker calculation
    Additional special dice values are now being correctly recognized and
    the calculation of dealing/collection the correct  number of markers
    based on the dice values has been added.

--- csharp_2013/JuleRules/JuleKataTests/RulesTests.cs
+++ csharp_2013/JuleRules/JuleKataTests/RulesTests.cs
@@ -24,7 +24,7 @@ namespace JuleKataTests
         }
 
         [TestMethod]
-        public void ShouldTransformDiceResultIntoCorrectInteger()
+        public void TransformDiceResultIntoCorrectInteger()
         {
             Assert.AreEqual(621, Rules.CalculateDiceValue(1,6,2));
         }
@@ -34,5 +34,47 @@ namespace JuleKataTests
         {
             Assert.IsTrue(Rules.IsThree(111));
         }
+
+        [TestMethod]
+        public void Recognize421AsJule()
+        {
+            Assert.IsTrue(Rules.IsJule(421));
+        }
+
+        [TestMethod]
+        public void RecognizeHards()
+        {
+            Assert.IsTrue(Rules.IsHards(511));
+            Assert.IsTrue(Rules.IsHards(311));
+            Assert.IsFalse(Rules.IsHards(111));
+        }
+
+        [TestMethod]
+        public void RecognizeSofts()
+        {
+            Assert.IsTrue(Rules.IsSofts(222));
+            Assert.IsTrue(Rules.IsSofts(333));
+            Assert.IsFalse(Rules.IsSofts(111));
+        }
+
+        [TestMethod]
+        public void RecognizeStraight()
+        {
+            Assert.IsTrue(Rules.IsStraight(321), "321");
+            Assert.IsTrue(Rules.IsStraight(432), "432");
+        }
+
+        [TestMethod]
+        public void UseCorrectNumberOfMarkers()
+        {
+            Assert.AreEqual(8, Rules.GetMarkersForDiceValue(111), "Three");
+            Assert.AreEqual(7, Rules.GetMarkersForDiceValue(421), "Jule");
+            Assert.AreEqual(6, Rules.GetMarkersForDiceValue(611), "6 Hards");
+            Assert.AreEqual(3, Rules.GetMarkersForDiceValue(311), "3 Hards");
+            Assert.AreEqual(3, Rules.GetMarkersForDiceValue(222), "2 Softs");
+            Assert.AreEqual(2, Rules.GetMarkersForDiceValue(543), "Straight");
+            Assert.AreEqual(1, Rules.GetMarkersForDiceValue(521), "Other values");
+        }
+
     }
 }
--- csharp_2013/JuleRules/JuleRules/Rules.cs
+++ csharp_2013/JuleRules/JuleRules/Rules.cs
@@ -8,11 +8,23 @@ namespace JuleKata
 {
     public class Rules
     {
+        public const int SPECIAL_VALUE_THREE = 111;
+        public const int SPECIAL_VALUE_JULE = 421;
+
         public static int RollDie()
         {
             Random dieRoll = new Random();
 
             return dieRoll.Next(1, 6);
+        }
+
+        public static int CalculateDiceValue(int dieOneResult, int dieTwoResult, int dieThreeResult)
+        {
+            int[] singleDieValues = Rules.SortDice(dieOneResult, dieTwoResult, dieThreeResult);
+
+            string joinedDiceValues = String.Join("", new List<int>(singleDieValues).ConvertAll(i => i.ToString()).ToArray());
+
+            return Convert.ToInt32(joinedDiceValues);
         }
 
         public static int[] SortDice(int dieOneValue, int dieTwoValue, int dieThreeValue)
@@ -26,16 +38,63 @@ namespace JuleKata
 
         public static bool IsThree(int diceValue)
         {
-            return 111 == diceValue;
+            return SPECIAL_VALUE_THREE == diceValue;
         }
 
-        public static int CalculateDiceValue(int p1, int p2, int p3)
+        public static bool IsJule(int diceValue)
         {
-            int[] singleDieValues = Rules.SortDice(p1, p2, p3);
+            return SPECIAL_VALUE_JULE == diceValue;
+        }
 
-            string joinedDiceValues = String.Join("", new List<int>(singleDieValues).ConvertAll(i => i.ToString()).ToArray());
+        public static bool IsHards(int diceValue)
+        {
+            if (IsThree(diceValue))
+            {
+                return false;
+            }
 
-            return Convert.ToInt32(joinedDiceValues);
+            int lastTwoDice = diceValue % 100;
+
+            return lastTwoDice == 11;
+        }
+
+        public static bool IsSofts(int diceValue)
+        {
+            if (IsThree(diceValue))
+            {
+                return false;
+            }
+
+
+            return diceValue % 111 == 0;
+        }
+
+        public static bool IsStraight(int diceValue)
+        {
+            int dieOne = diceValue / 100;
+            int dieTwo = diceValue % 100 / 10;
+            int dieThree = diceValue % 100 % 10;
+
+            return (--dieOne == dieTwo) && (--dieTwo == dieThree);
+        }
+
+        public static int GetMarkersForDiceValue(int diceValue)
+        {
+            int markers = 0;
+
+            if (IsThree(diceValue)) markers = 8;
+            else if (IsJule(diceValue)) markers = 7;
+            else if (IsHards(diceValue)) markers = CalculateMarkersForHards(diceValue);
+            else if (IsSofts(diceValue)) markers = 3;
+            else if (IsStraight(diceValue)) markers = 2;
+            else markers = 1;
+            
+            return markers;
+        }
+
+        private static int CalculateMarkersForHards(int diceValue)
+        {
+            return diceValue / 100;
         }
     }
 }
